09a1f170ac40af1e9fbe1e99538cfc92652a51a3
Andrew Fomera <andrew@zerlex.net>
2015-12-05 00:18:40 -0600

    use the right things for Postgres database when looking up users

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,13 +18,14 @@ class User < ActiveRecord::Base
      end
    end
 
-
-  def self.find_for_database_authentication(warden_conditions)
-     conditions = warden_conditions.dup
-     if login = conditions.delete(:login)
-       where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-     else
-       where(conditions.to_hash).first
+   # Override find_for_database_authentication
+     # If you're not using PostgreSQL you may run into some problems here
+     def self.find_for_database_authentication(warden_conditions)
+         conditions = warden_conditions.dup
+         if login = conditions.delete(:login)
+           where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+         else
+           where(conditions.to_hash).first
+         end
      end
-   end
 end
