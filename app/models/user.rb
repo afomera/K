a486479742b1f3e177e082500bd7f3417bd35d21
Andrew Fomera <andrew@zerlex.net>
2015-12-05 00:15:45 -0600

    Allow login via Username or Email + Add views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,28 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  attr_accessor :login
+
+  validate :validate_username
+  # Only allow letter, number, underscore and punctuation.
+  validates :username, format: {message: "can only contain letters, numbers, underscores or dashes.", with: /\A[A-Za-z0-9\-\_]+\z/ }
+
+
+  private
+
+   def validate_username
+     if User.where(email: username).exists?
+       errors.add(:username, :invalid)
+     end
+   end
+
+
+  def self.find_for_database_authentication(warden_conditions)
+     conditions = warden_conditions.dup
+     if login = conditions.delete(:login)
+       where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+     else
+       where(conditions.to_hash).first
+     end
+   end
 end
