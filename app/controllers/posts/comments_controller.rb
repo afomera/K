22a6f269359898743e509a36582a80ff54b7d190
Andrew Fomera <andrew@zerlex.net>
2016-02-02 20:55:56 -0600

    Starting to add comments, still broken in the views / how things are rendered

--- app/controllers/posts/comments_controller.rb
+++ app/controllers/posts/comments_controller.rb
@@ -1,4 +1,32 @@
 class Posts::CommentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only: [:new, :create, :edit, :update]
+  before_action :set_comment, only: [:edit, :update]
+
   def new
+    @comment = @post.comments.new
   end
+
+  def create
+    @comment = @post.comments.create(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to post_path(@post), notice: 'Your comment has been added'
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
 end
